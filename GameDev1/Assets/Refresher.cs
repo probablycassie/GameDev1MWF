7daf69f3283da82544cdad5604817fa5d4e5a65f
probablycassie <probablycassie@gmail.com>
2025-09-03 13:50:17 -0400

    Update Refresher.cs

--- GameDev1/Assets/Refresher.cs
+++ GameDev1/Assets/Refresher.cs
@@ -2,28 +2,39 @@ using UnityEngine; //requires semicolon to end
 
 public class Refresher : MonoBehaviour
 {
-    int number = 5;
+    int option = 0;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        if (number != 10)
+        /*  switch (number)
+          {       
+              case 0:
+                  Debug.Log("Start");
+                  break;
+
+              case 1:
+                  Debug.Log("Options");
+                  break;
+              case 2:
+
+                  Debug.Log("Quit");
+                  break;
+          }*/
+
+        for (int i = 0; i < 5; i++)
         {
-            Debug.Log("True");
+            Debug.Log(i);
+            i++;
         }
-        else if (number > 5)
-        {
-            Debug.Log("Taco");
-        }
-        else
-        {
-            Debug.Log("False");
-        }
+        while (int < 5 || i != 3) ;
+
+        Debug.Log("End");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
